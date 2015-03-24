e8780e1b3ebe46a6322e0e537d2f5e2c7477adbd
marcus.levin <marcus.levin@elev.it-gymnasiet.se>
2015-03-24 14:32:35 +0100

    HELLO

---
+++ dices/uppgift1.rb
@@ -0,0 +1,31 @@
+require 'colorize'
+# randoms = 100
+#
+#
+#
+#
+# random.times do
+# dice = rand(1..6)
+#
+#
+# if (dice).abs == 6
+#
+#
+# end
+#
+#     end
+#
+# puts diff3/randoms.to_f
+
+(1..6).each do |dice|
+
+    if (dice1).abs == 6
+      diff3 += 1
+      print "#{dice}".red
+    else
+      print "#{dice}".blue
+    end
+  end
+  puts
+end
+
